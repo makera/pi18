fef9f148c1b08f65cacb97ba8bf00ffc13863353
makera <makars999mail@gmail.com>
2021-02-19 16:14:11 +0900

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+header ("Content-Type: text/html; charset=utf-8");
+?>
+<blockquote>
+<?php
+echo strftime('Сегодня %d-%m-%Y');
+?>
+</blockquote>
+asdfasd 
+asd fclose <dfn>asd fclose</dfn>
